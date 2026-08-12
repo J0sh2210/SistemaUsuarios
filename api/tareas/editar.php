c55f030576f9684d5d75272b1c1f7d312282536e
J0sh2210 <jramireza31@miumg.edu.gt>
2026-08-11 19:17:21 -0600

    fix: Funcionalidad editar, permite ahora el uso de ambos roles, separando usos

--- api/tareas/editar.php
+++ api/tareas/editar.php
@@ -1,7 +1,8 @@
 <?php
 
 require __DIR__ . "/../../config/conexion.php";
-require __DIR__ . "/../../middleware/rol_admin.php";
+require __DIR__ . "/../../middleware/auth.php";
+session_start();
 
 $json = file_get_contents("php://input");
 $datos = json_decode($json,true);
@@ -11,8 +12,28 @@ $Titulo = $datos["Titulo"];
 $Descripcion = $datos["Descripcion"];
 $Estado = $datos["Estado"];
 $FechaFin = $datos["FechaFin"];
+$IdUsuarioSesion = $_SESSION["IdUsuario"];
+header("Content-Type: application/json");
 
+if ($_SESSION["rol"] !== 1){
 $sql = "UPDATE Tarea SET
+     Estado =?
+    WHERE IdTarea = ? AND IdUsuario = ?
+";
+
+$stmt = $conexion -> prepare($sql);
+$resultado = $stmt -> execute([$Estado, $IdTarea, $IdUsuarioSesion]);
+if (!$resultado) {
+    echo json_encode([
+        "success" => false,
+        "message" => "La tarea no pudo ser editada, intentelo nuevamente"
+    ]);
+    exit;
+}
+}
+
+else {
+    $sql = "UPDATE Tarea SET
     IdUsuario = ?, Titulo = ?, Descripcion = ?, Estado =?, FechaFin = ?
     WHERE IdTarea = ?
 ";
@@ -31,3 +52,5 @@ echo json_encode([
     "success" => true,
     "message" => "Tarea editada correctamente"
 ]);
+
+}
